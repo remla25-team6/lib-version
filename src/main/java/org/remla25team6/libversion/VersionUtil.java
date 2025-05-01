f473306eefe30ff37908e0b8b6f0f9b6a12d4146
Sten van Wolfswinkel <s.k.vanwolfswinkel@student.tudelft.nl>
2025-05-01 15:00:22 +0200

    Add functionality to retrieve version

--- src/main/java/org/remla25team6/libversion/VersionUtil.java
+++ src/main/java/org/remla25team6/libversion/VersionUtil.java
@@ -1,4 +1,45 @@
 package org.remla25team6.libversion;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class VersionUtil {
+
+    private static final String PROPERTIES = "version.properties";
+    private static String libraryVersion = null;
+
+    /**
+     * Gets the version of this library.
+     * Only attempts to read file if it was not successfully retrieved before.
+     *
+     * @return The library version string or null if not found.
+     */
+    public static String getVersion() {
+        if (libraryVersion == null) {
+            libraryVersion = findVersion();
+        }
+        return libraryVersion;
+    }
+
+    /**
+     * Retrieves the version of the library from the version.properties file, which is populated by maven filtering.
+     *
+     * @return The library version string or null if not found.
+     */
+    public static String findVersion() {
+        Properties props = new Properties();
+        try (InputStream input = VersionUtil.class.getClassLoader().getResourceAsStream("version.properties")) {
+            if (input == null) {
+                return null;
+            }
+
+            props.load(input);
+            String version = props.getProperty("library.version");
+            return version;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
 }
